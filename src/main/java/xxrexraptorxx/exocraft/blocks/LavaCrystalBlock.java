d6d16788711ae8b29e17c1fe550c2edf706af467
XxRexRaptorxX <Raptor-Simon@web.de>
2023-11-06 18:15:04 +0100

    lava crystal block sets entities on fire

--- src/main/java/xxrexraptorxx/exocraft/blocks/LavaCrystalBlock.java
+++ src/main/java/xxrexraptorxx/exocraft/blocks/LavaCrystalBlock.java
@@ -1,12 +1,22 @@
 package xxrexraptorxx.exocraft.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 
 public class LavaCrystalBlock extends Block {
+
+	protected static final VoxelShape CUSTOM_COLLISION_AABB = Block.box(0.0625D, 0.0625D, 0.0625D, 15.9375D, 15.9375D, 15.9375D);
+
 
 	public LavaCrystalBlock() {
 		super(Properties.of()
@@ -16,6 +26,20 @@ public class LavaCrystalBlock extends Block {
 				.mapColor(MapColor.COLOR_RED)
 				.instrument(NoteBlockInstrument.BIT)
 		);
+	}
+
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+		return CUSTOM_COLLISION_AABB;
+	}
+
+
+	@Override
+	public void entityInside(BlockState state, Level level, BlockPos pos, Entity entityIn) {
+		if (!level.isClientSide) {
+			entityIn.setSecondsOnFire(5);
+		}
 	}
 
 }
