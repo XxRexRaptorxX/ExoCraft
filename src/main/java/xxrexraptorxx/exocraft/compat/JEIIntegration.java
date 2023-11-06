0703ae6f96040042b0427a29ae0e54795bcd3d8e
XxRexRaptorxX <Raptor-Simon@web.de>
2023-11-06 18:55:17 +0100

    jei integration added

---
+++ src/main/java/xxrexraptorxx/exocraft/compat/JEIIntegration.java
@@ -0,0 +1,41 @@
+package xxrexraptorxx.exocraft.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IIngredientManager;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import xxrexraptorxx.exocraft.main.References;
+import xxrexraptorxx.exocraft.registry.ModItems;
+
+import java.util.ArrayList;
+
+@JeiPlugin
+public class JEIIntegration implements IModPlugin {
+
+    private static final ResourceLocation ID = new ResourceLocation(References.MODID, "jei_plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registry) {
+        IIngredientManager ingredientManager = registry.getIngredientManager();
+
+        ArrayList<ItemStack> units = new ArrayList<ItemStack>();
+        units.add(new ItemStack(ModItems.ATLAS_UNIT.get()));
+        units.add(new ItemStack(ModItems.STRYDER_UNIT.get()));
+        units.add(new ItemStack(ModItems.ION_UNIT.get()));
+        units.add(new ItemStack(ModItems.LEGION_UNIT.get()));
+        units.add(new ItemStack(ModItems.OGRE_UNIT.get()));
+
+        registry.addIngredientInfo(units, VanillaTypes.ITEM_STACK, Component.translatable("message.exocraft.units_jei_desc"));
+
+    }
+}
