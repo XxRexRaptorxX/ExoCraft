5422e37b70cb625d46ceb3c7c895265af1ed3373
XxRexRaptorxX <Raptor-Simon@web.de>
2024-02-13 01:18:09 +0100

    loot gen fixed

--- src/main/java/xxrexraptorxx/exocraft/main/Exocraft.java
+++ src/main/java/xxrexraptorxx/exocraft/main/Exocraft.java
@@ -3,13 +3,16 @@ package xxrexraptorxx.exocraft.main;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 import xxrexraptorxx.exocraft.registry.CreativeModeTabs;
 import xxrexraptorxx.exocraft.registry.ModBlocks;
 import xxrexraptorxx.exocraft.registry.ModItems;
 import xxrexraptorxx.exocraft.utils.Config;
+import xxrexraptorxx.exocraft.world.LootTableInjection;
 
 /**
  * @author XxRexRaptorxX (RexRaptor)
@@ -22,10 +25,18 @@ public class Exocraft {
 
     public Exocraft() {
         MinecraftForge.EVENT_BUS.register(this);
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modBus.addListener(this::setup);
 
         Config.init();
         ModItems.init();
         ModBlocks.init();
         CreativeModeTabs.init();
+    }
+
+
+    private void setup(final @NotNull FMLCommonSetupEvent event) {
+        MinecraftForge.EVENT_BUS.addListener(LootTableInjection::onChestLootLoad);
     }
 }
